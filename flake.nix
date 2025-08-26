52239a87482b1bf7bdd057c0c941c5c821a25ff8
regular <jan@lagomorph.de>
2025-08-26 20:17:55 +0200

    Patch ssb-msgs te fix crash during array indexing

--- flake.nix
+++ flake.nix
@@ -11,25 +11,39 @@
       inherit system;
       pkgs = nixpkgs.legacyPackages.${system};
     });
+
+    owner = "regular";
+    repo = "tre-cli";
+    pname = repo;
+    version = "dc4ea24";
+
   in {
-    packages = eachSystem ( { pkgs, system }: {
+    packages = eachSystem ( { pkgs, system }: let
+      src = pkgs.fetchFromGitHub {
+        inherit owner repo;
+        rev = version;
+        sha256 = "sha256-7hCRHV1Xr6tBK85WPfwQ6ZNZnfxFXWic7bA1pIifRD4=";
+      };
+    in {
+      deps = pkgs.fetchNpmDeps {
+        inherit src;
+        hash = "sha256-VPJpjNS/XhaoBAoWV5pkUSel1M/vrE9ogg1j7WxvUdw=";
+      };
       default = pkgs.buildNpmPackage rec {
-        owner = "regular";
-        repo = "tre-cli";
-        version = "dc4ea24";
-
-        pname = repo;
-
-        src = pkgs.fetchFromGitHub {
-          inherit owner repo;
-          rev = version;
-          sha256 = "sha256-7hCRHV1Xr6tBK85WPfwQ6ZNZnfxFXWic7bA1pIifRD4=";
-        };
+        inherit src version pname;
 
         npmDepsHash = "sha256-VPJpjNS/XhaoBAoWV5pkUSel1M/vrE9ogg1j7WxvUdw=";
         makeCacheWritable = true;
         npmFlags = [ "--omit=dev" "--omit=optional"];
         dontNpmBuild = true;
+
+        postBuild = ''
+          # Find the dependency dir robustly (handles hoisting/nesting)
+          dep_dir=$(node -e "console.log(require.resolve('ssb-msgs/package.json').replace(/package\.json$/, '''))")
+
+          echo "Patching $dep_dir"
+          patch -p1 -d "$dep_dir" < ${./patches/ssb-msgs.patch}
+        '';
 
         meta = {
           description = "Command-line tools for tre and treos (for developing ssb apps)";
